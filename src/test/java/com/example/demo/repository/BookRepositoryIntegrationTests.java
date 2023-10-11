f809c028ae7718df74f2a1145eee55d9785c6bdf
Rakshit Kadam <rakshitkdm@gmail.com>
2023-10-11 15:06:06 +0530

    Using JPA & Hibernate to play with Persistence layer

---
+++ src/test/java/com/example/demo/repository/BookRepositoryIntegrationTests.java
@@ -0,0 +1,39 @@
+package com.example.demo.repository;
+
+import com.example.demo.TestDataUtil;
+import com.example.demo.domain.Author;
+import com.example.demo.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class BookRepositoryIntegrationTests {
+
+    private BookRepository underTest;
+
+    @Autowired
+    public BookRepositoryIntegrationTests(BookRepository underTest) {
+        this.underTest = underTest;
+    }
+
+    @Test
+    public void testThatBookCanBeCreatedAndRecalled() {
+        Author author = TestDataUtil.createTestAuthorA();
+        Book book = TestDataUtil.createTestBookA(author);
+        underTest.save(book);
+        Optional<Book> result = underTest.findById(book.getIsbn());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(book);
+    }
+
+}
